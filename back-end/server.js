5bff3998e5961d3a98054730b678f722409d74a5
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-07-18 20:55:51 +0300

    Cors implementation

--- back-end/server.js
+++ back-end/server.js
@@ -7,6 +7,13 @@ const PORT = 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const corsOptions = {
+    origin: ['http://localhost:3000', 'http://frontend:3000', 'http://127.0.0.1:3000', process.env.FRONTEND_URL],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.get("/", (req, res) => {
     res.status(200).json({ message: "Welcome to Everything is OK API", version: "1.0.0" });
 })
