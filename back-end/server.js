0f851278d0817d3ace96a51d6f89f5154b0376a0
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-07-19 14:35:28 +0300

    I would need these

--- back-end/server.js
+++ back-end/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
 
 const PORT = 8080;
 
