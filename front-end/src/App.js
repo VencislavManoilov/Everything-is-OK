af62b4755349b77895cc7881510ff702e9030466
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-07-21 17:11:11 +0300

    Checks the user

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,17 +4,51 @@ import axios from "axios";
 
 const URL = process.env.REACT_APP_BACKEND_URL || process.env.REACT_APP_CUSTOM_BACKEND_URL || "http://localhost:8080";
 
-const InitComponent = () => {
-    return (
-        <div>Hello World!</div>
+const InitComponent = ({ user }) => {
+    return user ? (
+        <div>
+            <div>{ user.guest ? "Guest" : "User" }</div>
+            { !user.guest ? (
+                    <div>
+                        <div>{ user.username }</div>
+                        <div>{ user.email }</div>
+                    </div>
+                ) : (<></>)
+            }
+        </div>
+    ) : (
+        <div>Hello World</div>
     )
 }
 
 function App() {
+    const [user, setUser] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const checkLoginStatus = async () => {
+            try {
+                const response = await axios.get(URL+"/user", { withCredentials: true });
+                if (response.data) {
+                    setUser(response.data.user);
+                    setLoading(false);
+                }
+            } catch (error) {
+                setLoading(false);
+            }
+        }
+
+        checkLoginStatus();
+    }, []);
+
+    if(loading) {
+        return <div>loading...</div>
+    }
+
     return (
         <Router>
             <Routes>
-                <Route path='/' element={<InitComponent />} />
+                <Route path='/' element={<InitComponent user={user} />} />
             </Routes>
         </Router>
     );
