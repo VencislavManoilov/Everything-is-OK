a2e9e9291b869b1f29793e2b10c741371142429f
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-07-21 16:57:43 +0300

    Starting to work on the front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
+import axios from "axios";
+
+const URL = process.env.REACT_APP_BACKEND_URL || process.env.REACT_APP_CUSTOM_BACKEND_URL || "http://localhost:8080";
+
+const InitComponent = () => {
+    return (
+        <div>Hello World!</div>
+    )
+}
 
 function App() {
     return (
-        <div>Hello World</div>
+        <Router>
+            <Routes>
+                <Route path='/' element={<InitComponent />} />
+            </Routes>
+        </Router>
     );
 }
 
