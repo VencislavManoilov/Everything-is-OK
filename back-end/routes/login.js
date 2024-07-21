6cff37c5710809e78d01855058eb25e3092f01f0
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-07-21 17:54:49 +0300

    Bug fixed; Refactor on login route

--- back-end/routes/login.js
+++ back-end/routes/login.js
@@ -23,37 +23,37 @@ route.post(
         const { username, password } = req.body;
 
         req.db.query('SELECT * FROM users WHERE username = ?', [username], async (error, results) => {
-            if (error) throw error;
+            if(error) {
+                console.log(error);
+                return res.status(500).json({ error: "Internal server error" });
+            }
 
-            if (results.length === 0) {
+            if(results.length === 0) {
                 return res.status(401).json({ error: 'User not found' });
             }
 
             const user = results[0];
 
             const match = await bcrypt.compare(password, user.password);
-            if (!match) {
+            if(!match) {
                 return res.status(401).json({ error: 'Invalid password' });
             }
 
-            if (req.session.guest) {
-                // Update the guest account to regular account
-                req.db.query('UPDATE users SET username = ?, password = ?, guest = ? WHERE id = ?', 
-                    [username, await bcrypt.hash(password, 10), false, req.session.userId], (error) => {
-                    if (error) throw error;
-
-                    req.session.username = username;
-                    req.session.guest = false;
-
-                    res.status(200).json({ success: 'Guest account converted to regular account' });
+            if(req.session.guest) {
+                // Delete the guest account and login to regular account
+                req.db.query('DELETE FROM users WHERE id = ?', [req.session.userId], (error) => {
+                    if(error) {
+                        console.log(error);
+                        return res.status(500).json({ error: "Internal server error" });
+                    };
                 });
-            } else {
-                req.session.userId = user.id;
-                req.session.username = user.username;
-                req.session.guest = user.guest;
-
-                res.status(200).json({ success: 'Logged in' });
             }
+
+            req.session.userId = user.id;
+            req.session.username = user.username;
+            req.session.guest = user.guest;
+
+            res.status(200).json({ success: true });
         });
     }
 );
