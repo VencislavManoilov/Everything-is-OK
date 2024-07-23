be7e8c97ba1bd0e4782e991e34c0133dd1355a60
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-07-23 15:14:29 +0300

    Fixed the Profile page

--- front-end/src/components/Profile.js
+++ front-end/src/components/Profile.js
@@ -4,24 +4,16 @@ import axios from "axios";
 const URL = process.env.REACT_APP_BACKEND_URL || process.env.REACT_APP_CUSTOM_BACKEND_URL || "http://localhost:8080";
 
 const Profile = ({ user }) => {
-    const isGuest = user.guest;
+    const isGuest = user.guest ? true : false;
 
-    const handleRegister = () => {
-        window.location.href = "/register";
-    };
-
-    const handleLogin = () => {
-        window.location.href = "/login";
-    };
-
-    const handleLogout = async () => {
+    const logout = async () => {
         try {
             await axios.post(URL+"/logout", {}, { withCredentials: true });
             window.location.href = "/";
         } catch(err) {
-
+            alert("Error:", err);
         }
-    };
+    }
 
     return (
         <div className="container mt-5">
@@ -37,7 +29,6 @@ const Profile = ({ user }) => {
                             {!isGuest && (
                                 <>
                                 <p><strong>Email:</strong> {user.email}</p>
-                                <p><strong>Username:</strong> {user.username}</p>
                                 </>
                             )}
                             {isGuest && (
@@ -46,12 +37,12 @@ const Profile = ({ user }) => {
                             <div className="mt-3">
                                 {isGuest ? (
                                 <>
-                                    <button className="btn btn-success me-2" onClick={handleRegister}>Register</button>
-                                    <button className="btn btn-primary me-2" onClick={handleLogin}>Login</button>
-                                    <button className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">Logout</button>
+                                    <button className="btn btn-success me-2" data-bs-toggle="modal" data-bs-target="#modalRegister">Register</button>
+                                    <button className="btn btn-primary me-2" data-bs-toggle="modal" data-bs-target="#modalLogin">Login</button>
+                                    <button className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalGuestLogout">Logout</button>
                                 </>
                                 ) : (
-                                    <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
+                                    <button className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalLogout">Logout</button>
                                 )}
                             </div>
                         </div>
@@ -59,19 +50,73 @@ const Profile = ({ user }) => {
                 </div>
             </div>
 
-            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal fade" id="modalRegister" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Are you sure you want to logout?</h5>
+                            <h5 className="modal-title" id="exampleModalLabel">Have in mind!</h5>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
-                            <p>The data will be deleted</p>
+                            <p>Registerin will transfer the data to your new account.</p>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                            <button type="button" className="btn btn-danger" onClick={() => {handleLogout()}}>Logout</button>
+                            <button type="button" className="btn btn-success" onClick={() => {window.location.href = "/register"}}>Register</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="modal fade" id="modalLogin" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Warning!</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <p>Logging in will delete your current guest data.</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                            <button type="button" className="btn btn-primary" onClick={() => {window.location.href = "/login"}}>Login</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="modal fade" id="modalGuestLogout" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Warning!</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <p>Logging out will delete your current guest data.</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                            <button type="button" className="btn btn-danger" onClick={logout}>Logout</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="modal fade" id="modalLogout" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Warning!</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <p>Are you sure?</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                            <button type="button" className="btn btn-danger" onClick={logout}>Logout</button>
                         </div>
                     </div>
                 </div>
