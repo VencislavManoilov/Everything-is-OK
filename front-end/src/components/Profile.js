295131be9eb466f1a5f53b8ce7e0a4fcaa4e78f2
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-07-23 15:54:17 +0300

    Added a button for deleting the account

--- front-end/src/components/Profile.js
+++ front-end/src/components/Profile.js
@@ -15,6 +15,15 @@ const Profile = ({ user }) => {
         }
     }
 
+    const Delete = async () => {
+        try {
+            await axios.delete(URL+"/delete", { withCredentials: true });
+            window.location.href = "/";
+        } catch(err) {
+            alert("Error: " + err);
+        }
+    }
+
     return (
         <div className="container mt-5">
             <div className="card">
@@ -42,8 +51,10 @@ const Profile = ({ user }) => {
                                     <button className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalGuestLogout">Logout</button>
                                 </>
                                 ) : (
-                                    <button className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalLogout">Logout</button>
-                                )}
+                                <>
+                                    <button className="btn btn-danger me-2" data-bs-toggle="modal" data-bs-target="#modalLogout">Logout</button>
+                                    <button className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalDelete">Delete</button>
+                                </>)}
                             </div>
                         </div>
                     </div>
@@ -117,6 +128,24 @@ const Profile = ({ user }) => {
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
                             <button type="button" className="btn btn-danger" onClick={logout}>Logout</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="modal fade" id="modalDelete" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Warning!</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <p>Are you sure?</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                            <button type="button" className="btn btn-danger" onClick={Delete}>Delete</button>
                         </div>
                     </div>
                 </div>
