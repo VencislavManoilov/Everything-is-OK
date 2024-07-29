e891c8f1eb780c91bc92763802735c3e5f7706a8
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-07-29 15:49:12 +0300

    Added spinning thing when it is waiting in the Register too

--- front-end/src/components/Register.js
+++ front-end/src/components/Register.js
@@ -8,11 +8,13 @@ const Register = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         try {
+            setLoading(true);
             const response = await axios.post(URL+"/register", {
                 username: username,
                 email: email,
@@ -20,6 +22,7 @@ const Register = () => {
             }, { withCredentials: true });
 
             if(response.data.success) {
+                setLoading(false);
                 window.location.href = "/";
             }
         } catch(err) {
@@ -33,6 +36,16 @@ const Register = () => {
                 setError((err.response.data.error) ? err.response.data.error : "Login failed! Please check your information");
             }
         }
+    }
+
+    if(loading) {
+        return (
+            <div className="w-100 vh-100 text-center align-content-center">
+                <div className="spinner-border text-center mx-auto" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            </div>
+        );
     }
 
     return (
