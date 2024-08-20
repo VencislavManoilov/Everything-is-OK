96b7c6aae0ec41921fca05722a61d780497fcc9e
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-08-20 17:26:42 +0300

    Made the text input to work well on phone. Need to pull it on server. It is weird to type on mechanical keyboard after so long typing on laptop

--- front-end/src/components/Chat.js
+++ front-end/src/components/Chat.js
@@ -244,8 +244,8 @@ const Chat = ({ Chat, LoadChats }) => {
                 </div>
             </div>
 
-            <div className="row mx-3 justify-content-center">
-                <div className="container col p-0 text-center mb-2" style={{ maxWidth: "800px" }}>
+            <div className="row justify-content-center mx-3">
+                <div className="container col-12 col-md-8 p-0 text-center mb-2">
                     <div className="input-group bg-body-tertiary p-2" style={{ borderRadius: "26px" }}>
                         <textarea
                             ref={inputRef}
@@ -258,18 +258,16 @@ const Chat = ({ Chat, LoadChats }) => {
                             style={{ overflowY: 'hidden', resize: 'none' }}>
                         </textarea>
 
-                        <div title="Send" className="align-content-center rounded-end" style={{minHeight: "100%", minWidth: "36px"}}>
-                            <div className="input-group-append" style={{width: "30px", height: "30px"}}>
-                                <button className="btn btn-dark border-0 p-0 w-100 h-100" style={{borderRadius: "50%", transform: "translateX(2px)"}} onClick={handleSend}>
-                                    <svg width="20" height="20" fill="currentColor" style={{transform: "translateY(-2px)"}} viewBox="0 0 16 16">
-                                        <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
-                                    </svg>
-                                </button>
-                            </div>
+                        <div title="Send" className="d-flex align-items-center justify-content-center rounded-end" style={{ minWidth: "36px" }}>
+                            <button className="btn btn-dark border-0 p-0" style={{ borderRadius: "50%", width: "36px", height: "36px" }} onClick={handleSend}>
+                                <svg width="20" height="20" fill="currentColor" style={{ transform: "translateY(-2px)" }} viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
+                                </svg>
+                            </button>
                         </div>
                     </div>
 
-                    <span className="text-body-tertiary" style={{fontSize: "14px"}}>AI can make mistakes. Check important info.</span>
+                    <span className="text-body-tertiary" style={{ fontSize: "14px" }}>AI can make mistakes. Check important info.</span>
                 </div>
             </div>
         </div>
